5d9962ad7ec5159cf8f514be6510f63801d5397e
calvinho <ch235@cornell.edu>
2024-11-07 13:21:23 -0800

    add missing files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jitsidemo.restvo.com',
+  appName: 'jitsi-meet-demo',
+  webDir: 'dist'
+};
+
+export default config;
